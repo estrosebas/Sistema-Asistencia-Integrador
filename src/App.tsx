ab2f99a3bc82c1a12daab8808eed53ecb22e4533
Danny <koenigseggagera036@gmail.com>
2024-10-15 22:12:25 -0500

    feat(UI): desarrollo de la pantalla de login

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import LoginComponent from "./Login/LoginComponent";
+import Home from "./Home/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<LoginComponent />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
