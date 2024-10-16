cdbe671c34690ee6eb6de4a5a5ef1ad8f8890406
Diego Sebastian Gonzales Gomez <72907831+estrosebas@users.noreply.github.com>
2024-10-16 04:27:54 -0500

    feat(UI): desarrollo de la pantalla de administracion

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import LoginComponent from "./Login/LoginComponent";
+import Register from "./Register/Register";
+import AdminPanel from "./Admin/AdminPanel";
 import Home from "./Home/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,6 +10,9 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<LoginComponent />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Admin" element={<AdminPanel />} />
       </Routes>
     </Router>
   );
