9d78247a0a67918ef2b4c2a5fd7362e3f0a89863
winsthonapazamamani <winsthonapazamamani@gmail.com>
2024-10-30 09:51:25 -0500

    React panel Manager

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import LoginComponent from "./Login/LoginComponent";
 import Register from "./Register/Register";
+import AdminPanel from "./Admin/AdminPanel";
 import Home from "./Home/Home";
+import UserPanel from "./User/UserPanel";
+import ManagerPanel from "./Manager/ManagerPanel";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -11,6 +15,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Login" element={<LoginComponent />} />
         <Route path="/Register" element={<Register />} />
+        <Route path="/Admin" element={<AdminPanel />} />
+        <Route path="/User" element={<UserPanel />} />        
+        <Route path="/Manager" element={<ManagerPanel/>} />
       </Routes>
     </Router>
   );
